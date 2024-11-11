b84e2efde131496fc995da25b9692328ca613caa
PFonseca <pedro.fonseca01@utrgv.edu>
2024-11-11 12:35:11 -0600

    Registration_2 / debug errors

--- testApp/forms.py
+++ testApp/forms.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.forms import UserCreatinForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from djando import forms
+from django import forms
 
-class signUpForm(UserCreatinForm):
+class SignUpForm(UserCreationForm):
 	email = forms.EmailField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Type your email'}))
-	first_name = forms.CharField(label="",max_lenght=50,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}))
-	last_name = forms.CharField(label="",max_lenght=50,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}))
+	first_name = forms.CharField(label="",max_length=100,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}))
+	last_name = forms.CharField(label="",max_length=100,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}))
 
 	class Meta:
 		model = User
